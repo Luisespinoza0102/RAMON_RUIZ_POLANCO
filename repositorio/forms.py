f6fb23b86046734b6dac45151591cf078b5e50cc
Luisespinoza0102 <Luisjoseespinozacuicas17@gmail.com>
2026-06-10 06:04:18 -0400

    modificacion en forms repositorio

--- repositorio/forms.py
+++ repositorio/forms.py
@@ -10,12 +10,25 @@ class CategoriaForm(forms.ModelForm):
         }
 
 class DocumentoForm(forms.ModelForm):
+    archivo_pdf = forms.FileField(
+        required=True,
+        widget=forms.ClearableFileInput(attrs={'accept': '.pdf'})
+    )
+    portada = forms.ImageField(
+        required=False,
+        widget=forms.ClearableFileInput(attrs={'accept': 'image/*'})
+    )
     class Meta:
         model = DocumentoDigital
-        fields = ['titulo', 'autor', 'descripcion', 'categoria', 'archivo_pdf', 'portada']
+        fields = ['titulo', 'autor', 'descripcion', 'categoria']
         widgets = {
             'descripcion': forms.Textarea(attrs={'rows':3, 'class': 'input-modern'}),
             'titulo': forms.TextInput(attrs={'class': 'input-modern'}),
             'autor': forms.TextInput(attrs={'class': 'input-modern'}),
             'categoria': forms.Select(attrs={'class': 'input-modern'})
         }
+    def clean_archivo_pdf(self):
+        return self.cleaned_data.get('archivo_pdf')
+
+    def clean_portada(self):
+        return self.cleaned_data.get('portada')
